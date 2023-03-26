84dc2f537f12e657f7831e5037b7961080071a6e
Bartek <97710444+kieronsky@users.noreply.github.com>
2023-03-26 20:10:25 +0200

    small update
    added invulnerability

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    [Header("Health")]
     [SerializeField] private float startingHealth;
     public float currentHealth {  get; private set; }
     private Animator anim;
+    private bool dead;
 
+    [Header("iFrames")]
+    [SerializeField] private float invulnerablityDuration;
+    [SerializeField] private int numberOfFlashes;
+    private SpriteRenderer spriteRenderer;
     private void Awake()
     {
         currentHealth = startingHealth;
         //anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     public void TakeDamage(float damage)
@@ -21,6 +28,7 @@ public class Health : MonoBehaviour
         if(currentHealth > 0)
         {
             //anim.SetTrigger("Hurt");
+            StartCoroutine(Invulnerability());
         }
         else
         {
@@ -40,5 +48,18 @@ public class Health : MonoBehaviour
         {
             TakeDamage(1);
         }
+    }
+
+    private IEnumerator Invulnerability()
+    {
+        Physics2D.IgnoreLayerCollision(6, 7, true);
+        for (int i = 0; i < numberOfFlashes; i++)
+        {
+            spriteRenderer.color = new Color(1, 1, 1, 0.5f);
+            yield return new WaitForSeconds(invulnerablityDuration / (numberOfFlashes * 2));
+            spriteRenderer.color = Color.white;
+            yield return new WaitForSeconds(invulnerablityDuration / (numberOfFlashes * 2));
+        }
+        Physics2D.IgnoreLayerCollision(6, 7, false);
     }
 }
